1be449eb53385131988cef87bb2ec73f474754e1
chubarev.s <chubarev_sergey@mail.ru>
2023-03-29 00:14:38 +0500

    hm2t10

---
+++ HM2Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Ввод числа
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+
+int firstNum = (int)(num/100);          
+int secondNum = (int)(num/10 - firstNum*10);                
+
+
+
+Console.WriteLine(secondNum);
