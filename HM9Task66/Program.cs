2c16359c7fe25e252b2871a4b1f7bac750ee2249
chubarev.s <chubarev_sergey@mail.ru>
2023-04-23 21:51:45 +0500

    HM9

---
+++ HM9Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//Ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+//Рекур.метод
+int RecMN(int m, int n)
+{
+
+    if (m < n)
+    {
+        return m = m + RecMN(m + 1, n);
+    }
+    else
+    {
+        return m;
+    }
+}
+
+int M = ReadData("Введите число M: ");
+int N = ReadData("Введите число N: ");
+int MN = (M > N) ? RecMN(N, M) : RecMN(M, N);
+Console.Write(MN);
---
+++ HM9Task68/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+//Ввод данных
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+//Метод формирования функции Аккермана:
+long AckermanFunction(long m, long n)
+{
+    while(m!=0)
+    {
+        if(n==0)
+        {
+            n++;
+        }
+        else
+        {
+            n = AckermanFunction(m, n - 1);
+        }
+        m--; 
+    }
+    return n+1;
+}
+
+long M = ReadData("Введите число M: ");
+long N = ReadData("Введите число N: ");
+if(M<0||N<0)
+{
+    Console.WriteLine("Введите положительные целые числа!");
+}
+else
+{
+    long MN = AckermanFunction(M, N);
+    Console.WriteLine($"Значение функции Аккермана: {MN}");
+}
+
+
+
+
+
+
+
+
+
